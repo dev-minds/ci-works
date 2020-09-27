1d171332cc2aad143e33cb1fb39309e45b89b4a0
Felix Mbah <felixm@ukhoitmb3019.home>
2020-09-27 09:36:39 +0100

    Src code introduced

---
+++ app/calculus/src/main/java/com/fmbah/calculus/Calculus.java
@@ -0,0 +1,9 @@
+package com.fmbah.calculus;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Calculus {
+     int sum(int a, int b) {
+          return a + b;
+     }
+}
---
+++ app/calculus/src/main/java/com/fmbah/calculus/CalculusController.java
@@ -0,0 +1,17 @@
+package com.fmbah.calculus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class CalculatorController {
+     @Autowired
+     private Calculator calculus;
+
+     @RequestMapping("/sum")
+     String sum(@RequestParam("a") Integer a, 
+                @RequestParam("b") Integer b) {
+          return String.valueOf(calculus.sum(a, b));
+     }
+}
